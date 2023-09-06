89c7974812d6685b92e94d4c45f9b172fc55b5bf
Ganesh <ganeshnikam707@gmail.com>
2023-09-06 08:49:20 +0530

    First Commit

--- Queue.py
+++ Queue.py
@@ -4,24 +4,26 @@ def enqueue(a):
 def dequeue(a):
     if len(a)==0:
      print("Queue Underflow: ")
+    else:
+        item=a.pop(0)
+        print("dDequeued item=" ,item)
 def peek(a):
     if len (a) ==0:
         print("Queue Underflow: ")
     else:
         item = a[0]
-        print("peek item= ",item)
+        print("Peek item= ",item)
 def display(a):
     if len(a)==0:
         print("Queue Underflow: ")
     else:
-        for i in range(0,len,(a)):
+        for i in range(0,len(a)):
              print(a[i])
-
 
 #__main()__
 a=[]
 while True:
-    choice= int(input("\n1-Enqueue\n2-Dequeue\n3-Peek\n4-Display\n5-Exit\nEnter"))
+    choice= int(input("\n1-Enqueue\n2-Dequeue\n3-Peek\n4-Display\n5-Exit\nEnter Number ->"))
     if choice==1:
         enqueue(a)
     elif choice==2:
